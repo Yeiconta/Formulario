2ad37809651fb0d80a9f84893bf9a39c7bb93e61
Yeiconta <41449352+Yeiconta@users.noreply.github.com>
2018-08-02 19:55:51 -0500

    Initial commit

---
+++ Formulario/Formulario/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Formulario
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void Continue_Tapped(object sender, EventArgs e)
+        {
+            if (await validarFormulario())
+            {
+                await DisplayAlert("Exito", "Todos Tus campos cumplieron la validacion", "OK");
+            }
+        }
+        private async Task<bool> validarFormulario()
+        {
+            if (String.IsNullOrWhiteSpace(UserName.Text))
+            {
+                await this.DisplayAlert("Advertencia", "El campo es obligatorio", "OK");
+                return false;
+            }
+            else if (!UserName.Text.ToCharArray().All(char.IsLetter))
+            {
+                await this.DisplayAlert("Advertencia", "Tu informacion contiene numeros, favor validar.", "OK");
+                return false
+            }
+        }
+    
+       
+    }
+}
